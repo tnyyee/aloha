fb6aeb116af80fc56d00e90b05b179345d176909
Lovisa Carlsson <lovisacarlssons@gmail.com>
2018-05-15 21:22:19 +0200

    added dashboard

--- Aloha/imports/ui/components/dashboard/dashboard.js
+++ Aloha/imports/ui/components/dashboard/dashboard.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import {withHistory, Link} from 'react-router-dom';
-
+import '../../../../client/main.css';
 
 export const Dashboard = () =>
-  <div>
-  <p>this works</p>
-  </div>
+
+<div>
+
+
+  <section id="first_section" className ="dashboard_section">
+    <h1>ALOHA!</h1>
+    <button className="large_button">Alla trådar</button>
+    <h2>Aktuella trådar</h2>
+
+  <div className="row">
+      <div className="column">
+        <img src="vectors/dashboard/cam_ana_skandal.svg"/>
+        <img src="vectors/dashboard/knytblus.svg" id="second_img"/>
+        <img src=""></img>
+      </div>
+      <div className="column">
+        <img src="vectors/dashboard/stora_traden_om_aktier.svg"/>
+        <img src="vectors/dashboard/stora_traden_om_java.svg"/>
+        <img src=""></img>
+      </div>
+    </div>
+
+  </section>
+  <section className ="dashboard_section">
+  </section>
+  <section className ="dashboard_section">
+  </section>
+  <section className ="dashboard_section">
+  </section>
+  <section className ="dashboard_section">
+  </section>
+
+</div>
