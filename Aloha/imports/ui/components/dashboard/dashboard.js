3ceb4c018dbd67c8fc5556a1cf60f04e9508f7a3
Lovisa Carlsson <lovisacarlssons@gmail.com>
2018-05-25 16:40:26 +0200

    updated dashboard

--- Aloha/imports/ui/components/dashboard/dashboard.js
+++ Aloha/imports/ui/components/dashboard/dashboard.js
@@ -13,6 +13,7 @@ import ReactVote from 'react-vote';
 
 
 
+
 import './style_dashboard.css';
 import '../../../../client/main.css';
 
@@ -32,6 +33,11 @@ export class Dashboard extends Component{
   onCloseModal = () => {
     this.setState({ open: false });
   };
+
+  onCreate (data, diff) {
+    // Save data into the  databasehere!
+   console.log(data); // { title: 'title of this vote', items: [{ title: 'option1', count: 5, voters: ['a', 'b', 'c', 'd', 'e'] }, { title: 'option2', count: 3, voters: ['f', 'g', 'h'] }], closed: false, multiple: false, expansion: false, voters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'] }
+  }
 
 
   render() {
@@ -100,12 +106,10 @@ export class Dashboard extends Component{
           <Popup/>
           <img className="pineapple" src="vectors/pineapple.svg" />
         </section>
-        <div className="page_break"></div>
+        {/*<div className="page_break"></div>
         <section id="fifth_section_dashboard" className ="dashboard_section">
           <h2>Veckans omröstning</h2>
-
-
-        </section>
+        </section>*/}
       </div>
     );
   }
